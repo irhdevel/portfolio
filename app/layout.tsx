766a6de05126b9f75645098e6402f8ff7175fbed
irhdevel <irihi.design@gmail.com>
2023-10-19 18:09:56 +0900

    [added] google verification for search console

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,15 +4,18 @@ import { RLayout } from '@/components/RLayout'
 
 
 export const metadata: Metadata = {
-  title: 'Irihi',
-  description: "Irihi\'s Portfolio",
+    title: 'Irihi',
+    description: "Irihi\'s Portfolio",
+    verification: {
+        google: "pSCseIhUeuRezlW0IRv1trmHrHajHPQPaI-n2uCcOX0"
+    }
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
-}) {
+        children: React.ReactNode
+    }) {
     return (
         <RLayout>
             {children}

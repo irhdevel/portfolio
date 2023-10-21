4ebd9dfe67586cb8d5f11e78e362299409a67f3a
irhdevel <irihi.design@gmail.com>
2023-10-21 22:51:42 +0900

    [added] next-sitemap configuration file

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://irihi.pages.dev',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    outDir: './out',
+};

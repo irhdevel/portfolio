89b868acee54060ced29c702232d7362e804dd89
irhdevel <irihi.design@gmail.com>
2023-12-03 04:36:26 +0900

    [updated] packages to latest

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
     output: 'export',
     trailingSlash: true,
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.m?js$/,
+            type: "javascript/auto",
+            resolve: {
+                fullySpecified: false,
+            },
+        })
+        return config
+    }
 }
 
 export default nextConfig;
